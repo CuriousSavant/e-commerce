2980fc34be52c3af15a92967181f93a3515890a6
Craft187 <this_account_for_backupCode_[0019]>
2024-12-07 13:09:06 +0700

    Fix: Resolve Prisma initialization issue

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient()
+const prisma = global.__prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") {
+  global.__prisma = prisma;
+}
 export default prisma;
